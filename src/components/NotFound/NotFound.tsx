1f51690d2a7a876248acb050b45634ed1fd66425
skychaser <misha.kirillov.0990@gmail.com>
2024-07-15 02:32:02 +0300

    feat: make a 404 page

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+export function NotFound(): JSX.Element {
+  return (
+    <div className="not-found">
+      <h1>404 - Page Not Found</h1>
+      <Link to="/search/1">Go to Search Page</Link>
+    </div>
+  );
+}
