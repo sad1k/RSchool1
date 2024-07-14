acf8dd22a22bc2f3ded2fc377dd79f7622a5192f
skychaser <misha.kirillov.0990@gmail.com>
2024-07-15 02:31:05 +0300

    feat: make a title with empty results

--- src/components/SearchPage/SearchList/SearchList.tsx
+++ src/components/SearchPage/SearchList/SearchList.tsx
@@ -3,13 +3,17 @@ import "./styles.css";
 
 interface IProps {
   results: Array<ISearchItem> | null;
+  onItemClick: (id: string) => void;
 }
 
-export function SearchList({ results }: IProps): JSX.Element {
+export function SearchList({ results, onItemClick }: IProps): JSX.Element {
   return (
     <div className="person-list">
+      {!results && <p>No such results</p>}
       {results &&
-        results.map((item) => <SearchItem key={item.name} person={item} />)}
+        results.map((item) => (
+          <SearchItem onItemClick={onItemClick} key={item.name} person={item} />
+        ))}
     </div>
   );
 }
