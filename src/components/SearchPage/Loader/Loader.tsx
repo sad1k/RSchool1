44771e22305e66194bcf543e050cf016b6396814
sad1k <91619133+sad1k@users.noreply.github.com>
2024-07-14 15:56:19 +0300

    Revert "Class components"

--- src/components/SearchPage/Loader/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, ReactNode } from "react";
-import "./styles.css";
-
-export class Loader extends Component {
-  render(): ReactNode {
-    return <div className="loader">LOADING...</div>;
-  }
-}
