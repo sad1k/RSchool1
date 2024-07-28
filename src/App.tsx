3ecf5139b753be0773c44bf46cca722974aced84
skychaser <misha.kirillov.0990@gmail.com>
2024-07-28 19:53:34 +0300

    feat: app add flyout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { Component } from "react";
 import { SearchPage } from "./components/SearchPage/SearchPage";
-import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
 import "./index.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { NotFound } from "./components/NotFound/NotFound";
 import { DetailSection } from "./components/SearchPage/SearchPageItem/DetailSection/DetailSection";
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
+import Flyout from "./components/SearchPage/Flyout/Flyout";
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <ErrorBoundary>
-          <Routes>
-            <Route path="/search/:page" element={<SearchPage />}>
-              <Route path=":detailsId" element={<DetailSection />} />
-            </Route>
-            <Route path="/" element={<Navigate to="/search/1" />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </ErrorBoundary>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/search/:page" element={<SearchPage />}>
+          <Route path=":detailsId" element={<DetailSection />} />
+        </Route>
+        <Route path="/" element={<Navigate to="/search/1" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <>
+        <Flyout />
+      </>
+    </ErrorBoundary>
+  );
 }
