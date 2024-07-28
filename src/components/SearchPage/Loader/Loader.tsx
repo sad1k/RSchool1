fceb4b8f71a0c219a127edb0acf38402f9169729
skychaser <misha.kirillov.0990@gmail.com>
2024-07-28 20:32:15 +0300

    refactor: add role for tests

--- src/components/SearchPage/Loader/Loader.tsx
+++ src/components/SearchPage/Loader/Loader.tsx
@@ -1,5 +1,9 @@
 import "./styles.css";
 
-export function Loader(): JSX.Element {
-  return <div className="loader">LOADING...</div>;
+export function Loader({ role }: { role: string }): JSX.Element {
+  return (
+    <div role={role} className="loader">
+      LOADING...
+    </div>
+  );
 }
