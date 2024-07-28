834054043b9c366b704fdb61ba9d8ccf85053b1a
skychaser <misha.kirillov.0990@gmail.com>
2024-07-28 19:45:27 +0300

    feat: make search bar with redux

--- src/components/SearchPage/SearchBar/SearchBar.tsx
+++ src/components/SearchPage/SearchBar/SearchBar.tsx
@@ -1,42 +1,42 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import "./styles.css";
+import Button from "../../../Button";
+import { ThemeContext } from "../../../ThemeContext/ThemeContext";
+import { useNavigate } from "react-router-dom";
+import { setSearchTerm } from "../../../store/cardsSlice";
+import { useDispatch } from "react-redux";
 
-interface IProps {
-  onSearch: (searchTerm: string) => void;
-  throwError: () => void;
-  searchTerm: string;
-  setSearchTerm: (term: string) => void;
-}
-
-export function SearchBar({
-  throwError,
-  onSearch,
-  searchTerm,
-  setSearchTerm,
-}: IProps): JSX.Element {
+export function SearchBar(): JSX.Element {
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = function (e: ChangeEvent<HTMLInputElement>): void {
-    setSearchTerm(e.target.value);
+    setSearch(e.target.value);
   };
 
+  const handleSearch = function (searchTerm: string) {
+    navigate(`/search/1`);
+    dispatch(setSearchTerm(searchTerm));
+  };
+
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="search-section">
-      <button
-        onClick={() => {
-          throwError();
-        }}
-        className="throw-error-btn"
-      >
-        Throw error
-      </button>
+    <div
+      role="search-section"
+      className={`search-section ${theme ? "darkTheme" : "lightTheme"}`}
+    >
       <input
+        role="search"
         className="input-bar"
         placeholder="Bulbasavar"
         onChange={(e) => handleChange(e)}
-        value={searchTerm}
+        value={search}
       />
-      <button className="search-btn" onClick={() => onSearch(searchTerm)}>
+      <button className="search-btn" onClick={() => handleSearch(search)}>
         Search
       </button>
+      <Button role="theme-btn" />
     </div>
   );
 }
