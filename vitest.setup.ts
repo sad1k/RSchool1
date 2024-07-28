df2ed51137bb019a61a31565393ca05576dc28f9
skychaser <misha.kirillov.0990@gmail.com>
2024-07-28 19:57:07 +0300

    feat: make setup file for all tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,20 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { setupServer } from "msw/node";
+import { handlers } from "./src/mocks/api/handlers";
 
 afterEach(() => {
   cleanup();
 });
+
+export const server = setupServer(...handlers);
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+//  Close server after all tests
+afterAll(() => server.close());
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers());
