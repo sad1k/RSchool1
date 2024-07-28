5f0b77903f9e8e46811f239115e3a763582db28b
skychaser <misha.kirillov.0990@gmail.com>
2024-07-28 19:44:40 +0300

    feat: make pagination with redux state

--- src/components/SearchPage/Pagination/Pagination.tsx
+++ src/components/SearchPage/Pagination/Pagination.tsx
@@ -1,28 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { countPages } from "../../../utils/countPages";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../../store/store";
+import { useDispatch } from "react-redux";
+import { setCurrentPage } from "../../../store/cardsSlice";
 
-interface IProps {
-  maxCount: number;
-  onChangePage: (page: number) => void;
-  currentPage: number;
-}
-
-export function Pagination({
-  currentPage,
-  maxCount,
-  onChangePage,
-}: IProps): JSX.Element {
+export function Pagination(): JSX.Element {
+  const maxCount = useAppSelector((state) => +state.cards.maxCountCards);
+  const currentPage = useAppSelector((state) => state.cards.currentPage);
+  const dispatch = useDispatch();
   const [pages, setPages] = useState(
     countPages(currentPage, Math.ceil(maxCount / 10)),
   );
 
+  useEffect(() => {
+    setPages(countPages(currentPage, Math.ceil(maxCount / 10)));
+  }, [maxCount, currentPage]);
+
+  const navigate = useNavigate();
+
+  const handlePageChange = function (page: number) {
+    navigate(`/search/${page}`);
+    dispatch(setCurrentPage(page));
+  };
+
   const handleClick = function (page: number) {
     const maxPages: number = Math.ceil(maxCount / 10);
     setPages(countPages(page, maxPages));
-    onChangePage(page);
+    handlePageChange(page);
   };
-
   return (
     <div className="pagination">
       {pages.map((page) => (
