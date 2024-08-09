bb510a0cd5b36ae74708cda86d5b339e7ce950c3
skychaser <misha.kirillov.0990@gmail.com>
2024-08-09 14:52:27 +0300

    refactor: delete src directory

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { Detail } from "../components/SearchPage/SearchPageItem/DetailSection/DetailSection";
-
-export interface ApiResponse {
-  count: number;
-  next: string;
-  previous: string | null;
-  results: Array<Detail>;
-}
--- src/components/SearchPage/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export function Loader({ role }: { role: string }): JSX.Element {
-  return (
-    <div role={role} className="loader">
-      LOADING...
-    </div>
-  );
-}
